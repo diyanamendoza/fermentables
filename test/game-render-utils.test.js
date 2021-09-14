b320e7f3fe097dfd5d8feda49aaf734312926988
Dylan Floyd <floyd800@gmail.com>
2021-09-14 13:58:35 -0700

    Change renderActionButtons and add renderFermInfo

--- test/game-render-utils.test.js
+++ test/game-render-utils.test.js
@@ -1,11 +1,12 @@
 import { renderActionButtons, renderFFOneDayButton, renderFFOneWeekButton } from '../game/game-render-utils.js';
 import { setGameData } from '../local-storage-utils.js';
+import { setDataForGetAllActionNames } from '../utils.js';
 
 const test = QUnit.test;
 
 test('renderActionButtons renders a div with a button for each ferm action', assert => {
-    const testData = {
-        activeFerms: [{
+    const testData = [
+        {
             id: 1,
             actions: [
                 {
@@ -38,8 +39,8 @@ test('renderActionButtons renders a div with a button for each ferm action', ass
                     action: 'qwer4'
                 }
             ]
-        }]
-    };
+        }
+    ];
     const expected = 
     '<div id="action-buttons-div">' +
         '<button value="asdf1" class="action-button">asdf1</button>' +
@@ -51,6 +52,7 @@ test('renderActionButtons renders a div with a button for each ferm action', ass
         '<button value="qwer3" class="action-button">qwer3</button>' +
         '<button value="qwer4" class="action-button">qwer4</button>' +
     '</div>';
+    setDataForGetAllActionNames(testData);
     setGameData(testData);
     const actual = renderActionButtons().outerHTML;
     assert.equal(actual, expected);
