43482c6994c412db92a15850c136f7d2cdc44119
diyanamendoza <diyana.amp@pm.me>
2021-09-16 22:15:48 -0700

    Render nav on pantry pager too

--- pantry/pantry.js
+++ pantry/pantry.js
@@ -1,8 +1,8 @@
-import { renderFerms, renderPlayButton } from '../render-utils.js';
+import { renderFerms, renderNav, renderPlayButton } from '../render-utils.js';
 import { fermsTemplate } from '../fermentables-template.js';
 
+renderNav();
 renderFerms(fermsTemplate);
-
 renderPlayButton();
 
 
